3689acdbda651b839e7bd377a1e89978c362701b
Winston Zhang <winston.zhang@outlook.com>
2022-07-17 23:55:47 +1000

    Write function for calculating operators/operand

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ const equals = document.getElementById('equals');
 
 numbers.forEach(node => node.addEventListener('click', clickNumber));
 operators.forEach(node => node.addEventListener('click', clickOperator));
+equals.addEventListener('click', clickEquals);
 allClearButton.addEventListener('click', clear);
 
 let operations = {
@@ -16,12 +17,6 @@ let operations = {
     operator: null,
     last: null,
 }
-
-let add = (a, b) => a + b;
-let minus = (a, b) => a - b;
-let divide = (a, b) => a / b;
-let multiply = (a, b) => a * b;
-let modulo = (a, b) => a % b;
 
 function clickNumber() {
     let number = this.dataset.number;
@@ -42,10 +37,29 @@ function clickOperator() {
     console.log(operations);
 }
 
+function clickEquals() {
+    if (operations.last) {
+        console.log(doOperation());
+        upperDisplay.innerHTML += ` ${operations.current} =`;
+        lowerDisplay.innerHTML = doOperation();
+    }
+}
+
+function doOperation() {
+    operator = operations.operator;
+    a = operations.last;
+    b = operations.current;
+    if (operator === "+") return a + b; // plus
+    if (operator === "-") return a - b; // minus
+    if (operator === "+") return a / b; // divide
+    if (operator === "x") return a * b; // multiply
+    if (operator === "%") return a % b; // modulo
+}
+
 function clear() {
     lowerDisplay.innerHTML = "0";
     upperDisplay.innerHTML = "(empty)";
-    for (let key in operations) { operations[key] = null; } // reset operations keys to starting NULL value
+    for (let key in operations) { operations[key] = null; } // Change operations object keys to "null" value
     console.log(operations);
 }
 
