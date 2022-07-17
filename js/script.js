8ae3dd17fde306199f0f67c1bb7f9db593bec23b
Winston Zhang <winston.zhang@outlook.com>
2022-07-17 23:34:13 +1000

    Create functions for pressing operator buttons + update allClear function + write basic math functions

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 console.log("Calculator app");
 
 const numbers = document.querySelectorAll('.number');
+const upperDisplay = document.getElementById('upper-display-text');
 const lowerDisplay = document.getElementById('lower-display-text');
 const allClearButton = document.getElementById('all-clear');
+const operators = document.querySelectorAll('.operator');
+const equals = document.getElementById('equals');
 
 numbers.forEach(node => node.addEventListener('click', clickNumber));
+operators.forEach(node => node.addEventListener('click', clickOperator));
 allClearButton.addEventListener('click', clear);
 
 let operations = {
@@ -13,6 +17,12 @@ let operations = {
     last: null,
 }
 
+let add = (a, b) => a + b;
+let minus = (a, b) => a - b;
+let divide = (a, b) => a / b;
+let multiply = (a, b) => a * b;
+let modulo = (a, b) => a % b;
+
 function clickNumber() {
     let number = this.dataset.number;
     updateOperations('current', number);
@@ -20,14 +30,33 @@ function clickNumber() {
     console.log(operations.current);
 }
 
+function clickOperator() {
+    let operator = this.dataset.operator;
+    if (operations.current != null) {
+        operations.last = operations.current;
+        operations.operator = operator;
+        operations.current = null;
+        lowerDisplay.innerHTML = "0";
+        writeUpper();
+    }
+    console.log(operations);
+}
+
 function clear() {
-    lowerDisplay.innerHTML = '0';
+    lowerDisplay.innerHTML = "0";
+    upperDisplay.innerHTML = "(empty)";
     for (let key in operations) { operations[key] = null; } // reset operations keys to starting NULL value
+    console.log(operations);
 }
 
 function writeLower(string) {
-    if (lowerDisplay.innerHTML === "0") return lowerDisplay.innerHTML = string;
-    lowerDisplay.innerHTML += string;
+    lowerDisplay.innerHTML === "0" ? lowerDisplay.innerHTML = string : lowerDisplay.innerHTML += string;
+}
+
+function writeUpper() {
+    let number = operations.last;
+    let operator = operations.operator;
+    upperDisplay.innerHTML = `${number} ${operator}`;
 }
 
 function updateOperations(key, value) {
